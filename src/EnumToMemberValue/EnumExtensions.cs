930c973214aeca1b31d173af05942c92e0440137
Yuriy Durov <u.a.durov@gmail.com>
2022-01-03 17:28:22 +0300

    Add EnumToMemberValue

---
+++ src/EnumToMemberValue/EnumExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace BitzArt
+{
+    public static class EnumExtensions
+    {
+        public static string ToEnumMemberValue<TEnum>(this TEnum enumValue) where TEnum : struct, Enum
+        {
+            var member = typeof(TEnum).GetMember(enumValue.ToString()).First();
+            var attribute = member.GetCustomAttributes(false).OfType<EnumMemberAttribute>().Single();
+            return attribute.Value;
+        }
+    }
+}
---
+++ src/EnumToMemberValue/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace BitzArt
+{
+    public static class StringExtensions
+    {
+        public static TEnum ToEnum<TEnum>(this string enumString, TEnum? defaultValue = null) where TEnum : struct, Enum
+        {
+            var enumType = typeof(TEnum);
+            foreach (var name in Enum.GetNames(enumType))
+            {
+                var attributes = enumType.GetField(name).GetCustomAttributes(typeof(EnumMemberAttribute), true);
+                if (!attributes.Any()) continue;
+                var enumMemberAttribute = (attributes as EnumMemberAttribute[]).Single();
+                if (enumMemberAttribute.Value.ToLower() == enumString.ToLower()) return (TEnum)Enum.Parse(enumType, name);
+            }
+            if (defaultValue.HasValue) return defaultValue.Value;
+            throw new ArgumentException($"Can not parse '{enumString}' to enum '{typeof(TEnum).Name}'");
+        }
+    }
+}
