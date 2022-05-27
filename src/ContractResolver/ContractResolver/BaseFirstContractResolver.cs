077fe00de8c35b9d539db4d59dd3e0e2a38dcd79
Andrew Shpakovsky <volya93.as@gmail.com>
2022-05-27 13:53:35 +0400

    Added Contract resolver

---
+++ src/ContractResolver/ContractResolver/BaseFirstContractResolver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace ContractResolver
+{
+    public class BaseFirstContractResolver : DefaultContractResolver
+    {
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            var properties = base.CreateProperties(type, memberSerialization);
+            if (properties != null)
+                return properties.OrderBy(p => p.DeclaringType.BaseTypesAndSelf().Count()).ToList();
+            
+            return properties;
+        }
+
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            return char.ToLowerInvariant(propertyName[0]) + propertyName[1..];
+        }
+    }
+
+    public static class TypeExtensions
+    {
+        public static IEnumerable<Type> BaseTypesAndSelf(this Type type)
+        {
+            while (type != null)
+            {
+                yield return type;
+                type = type.BaseType;
+            }
+        }
+    }
+}
