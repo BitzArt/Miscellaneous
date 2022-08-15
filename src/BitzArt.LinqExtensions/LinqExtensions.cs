0455d24c8371a9d0416e8214598c7f0a8e8dbad5
Yuriy Durov <u.a.durov@gmail.com>
2022-08-15 15:25:38 +0400

    Add LinqExtensions

---
+++ src/BitzArt.LinqExtensions/LinqExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace System.Linq
+{
+    public static class LinqExtensions
+    {
+        public static IQueryable<TSource> WhereIf<TSource>(this IQueryable<TSource> source, bool condition, Expression<Func<TSource, bool>> predicate)
+        {
+            if (condition) return source.Where(predicate);
+            return source;
+        }
+    }
+}
