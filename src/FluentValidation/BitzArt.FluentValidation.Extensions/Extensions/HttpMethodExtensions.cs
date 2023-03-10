0e49d931b1c439595c8bec4ecc9907504371ab54
Yuriy Durov <u.a.durov@gmail.com>
2023-03-10 15:45:42 +0400

    Rework HttpValidator into ActionValidator

---
+++ src/FluentValidation/BitzArt.FluentValidation.Extensions/Extensions/HttpMethodExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace FluentValidation;
+
+public static class HttpMethodExtensions
+{
+    public static ActionType ToActionType(this HttpMethod httpMethod) => httpMethod.Method.ToActionType();
+
+    public static ActionType ToActionType(this string method) => method switch
+    {
+        "GET" => ActionType.Get,
+        "POST" => ActionType.Create,
+        "PUT" => ActionType.Update,
+        "PATCH" => ActionType.Patch,
+        "OPTIONS" => ActionType.Options,
+        "DELETE" => ActionType.Delete,
+        _ => throw new ArgumentException($"Unexpected httpMethod {method}")
+    };
+}
---
+++ src/FluentValidation/BitzArt.FluentValidation.Extensions/Validators/ActionValidator.cs
@@ -0,0 +1,26 @@
+﻿namespace FluentValidation;
+
+public abstract class ActionValidator<T> : JsonValidator<T>
+{
+    protected readonly ActionType ActionType;
+
+    protected ActionValidator(ActionType actionType)
+    {
+        ActionType = actionType;
+    }
+    public IConditionBuilder When(ActionType actionType, Action action)
+        => When(x => ActionType == actionType, action);
+
+    public IConditionBuilder Unless(ActionType actionType, Action action)
+        => Unless(x => ActionType == actionType, action);
+}
+
+public enum ActionType : byte
+{
+    Get = 1,
+    Create = 2,
+    Update = 3,
+    Patch = 4,
+    Options = 5,
+    Delete = 6
+}
