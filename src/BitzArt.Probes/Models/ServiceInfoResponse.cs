73c3e8e258f70512aeb9cd072c8aab7987c8935b
Yuriy Durov <u.a.durov@gmail.com>
2023-11-10 15:04:22 +0400

    Simplify ServiceName -> Name

--- src/BitzArt.Probes/Models/ServiceInfoResponse.cs
+++ src/BitzArt.Probes/Models/ServiceInfoResponse.cs
@@ -4,8 +4,8 @@ namespace BitzArt.Probes;
 
 internal class ServiceInfoResponse(ServiceInfoOptions? options, string? environment)
 {
-    [JsonPropertyName("serviceName")]
-    public string? ServiceName { get; set; } = options?.Name;
+    [JsonPropertyName("name")]
+    public string? Name { get; set; } = options?.Name;
 
     [JsonPropertyName("version")]
     public string? Version { get; set; } = options?.Version;
