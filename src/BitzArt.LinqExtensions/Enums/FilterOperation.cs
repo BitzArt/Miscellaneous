d41ef058ce458c673b922a0c700f6f0bbe1d201c
Yuriy Durov <u.a.durov@gmail.com>
2024-02-29 14:24:47 +0400

    Add FilterOperation

---
+++ src/BitzArt.LinqExtensions/Enums/FilterOperation.cs
@@ -0,0 +1,11 @@
+﻿namespace System.Linq;
+
+public enum FilterOperation : byte
+{
+    Equal = 0,
+    NotEqual = 1,
+    GreaterThan = 2,
+    GreaterThanOrEqual = 3,
+    LessThan = 4,
+    LessThanOrEqual = 5
+}
--- src/BitzArt.LinqExtensions/Extensions/AddFilterExtension.cs
+++ src/BitzArt.LinqExtensions/Extensions/AddFilterExtension.cs
@@ -5,15 +5,15 @@ namespace System.Linq;
 
 public static class AddFilterExtension
 {
-    public static IQueryable<TSource> AddFilter<TSource, TProperty>(this IQueryable<TSource> source, Expression<Func<TSource, TProperty?>> expression, TProperty? filter)
+    public static IQueryable<TSource> AddFilter<TSource, TProperty>(this IQueryable<TSource> source, Expression<Func<TSource, TProperty?>> expression, TProperty? filter, FilterOperation filterOperation = FilterOperation.Equal)
         where TProperty : class
     {
         if (filter is null) return source;
 
-        return BuildExpression(source, filter, expression);
+        return BuildExpression(source, filter, expression, filterOperation);
     }
 
-    public static IQueryable<TSource> AddFilter<TSource, TProperty>(this IQueryable<TSource> source, Expression<Func<TSource, TProperty?>> expression, TProperty? filter)
+    public static IQueryable<TSource> AddFilter<TSource, TProperty>(this IQueryable<TSource> source, Expression<Func<TSource, TProperty?>> expression, TProperty? filter, FilterOperation filterOperation = FilterOperation.Equal)
         where TProperty : struct
     {
         if (filter is null) return source;
@@ -22,15 +22,25 @@ public static class AddFilterExtension
         Expression<Func<TProperty?, TProperty>> getValueExpression = x => x!.Value;
         var valueExpression = expression.Compose(getValueExpression);
 
-        return BuildExpression(source, filterValue, valueExpression);
+        return BuildExpression(source, filterValue, valueExpression, filterOperation);
     }
 
-    private static IQueryable<TSource> BuildExpression<TSource, TProperty>(IQueryable<TSource> source, TProperty filter, Expression<Func<TSource, TProperty>> expression)
+    private static IQueryable<TSource> BuildExpression<TSource, TProperty>(IQueryable<TSource> source, TProperty filter, Expression<Func<TSource, TProperty>> expression, FilterOperation filterOperation)
     {
         var argument = Expression.Parameter(typeof(TSource));
         var left = Expression.Invoke(expression, argument);
         var right = Expression.Constant(filter);
-        var eq = Expression.Equal(left, right);
+
+        var eq = filterOperation switch
+        {
+            FilterOperation.Equal => Expression.Equal(left, right),
+            FilterOperation.NotEqual => Expression.NotEqual(left, right),
+            FilterOperation.GreaterThan => Expression.GreaterThan(left, right),
+            FilterOperation.GreaterThanOrEqual => Expression.GreaterThanOrEqual(left, right),
+            FilterOperation.LessThan => Expression.LessThan(left, right),
+            FilterOperation.LessThanOrEqual => Expression.LessThanOrEqual(left, right),
+            _ => throw new NotImplementedException($"Unsupported Filter Operation: '{filterOperation}'")
+        };
 
         var lambda = Expression
             .Lambda<Func<TSource, bool>>(eq, new[] { argument });
