9ecda33f9d0bdc68775a1a318d5c819518ecf068
Yuriy Durov <u.a.durov@gmail.com>
2024-04-02 14:27:11 +0400

    Add IfExtension

---
+++ src/BitzArt.LinqExtensions/Extensions/IfExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace System.Linq;
+
+public static class IfExtension
+{
+    public static IQueryable<T> If<T>(this IQueryable<T> source, bool condition, Expression<Func<IQueryable<T>, IQueryable<T>>> expression)
+    {
+        if (condition) return expression.Compile().Invoke(source);
+        return source;
+    }
+}
