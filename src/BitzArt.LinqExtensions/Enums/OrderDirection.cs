b656384b53d6a69804a67b376c8bc817c44f8454
Yuriy Durov <u.a.durov@gmail.com>
2024-04-16 15:05:10 +0400

    Add directional ordering

---
+++ src/BitzArt.LinqExtensions/Enums/OrderDirection.cs
@@ -0,0 +1,17 @@
+﻿namespace System.Linq;
+
+/// <summary>
+/// Order direction (ascending/descending).
+/// </summary>
+public enum OrderDirection : byte
+{
+    /// <summary>
+    /// Ascending order direction.
+    /// </summary>
+    Ascending = 0,
+         
+    /// <summary>
+    /// Descending order direction.
+    /// </summary>
+    Descending = 1
+}
---
+++ src/BitzArt.LinqExtensions/Extensions/OrderExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq.Expressions;
+
+namespace System.Linq;
+
+/// <summary>
+/// LINQ extensions for ordering queries.
+/// </summary>
+public static class OrderExtensions
+{
+    /// <summary>
+    /// Orders the query by the specified key selector and direction.
+    /// </summary>
+    public static IOrderedQueryable<TSource> OrderBy<TSource, TKey>(this IQueryable<TSource> query, Expression<Func<TSource, TKey>> keySelector, OrderDirection direction)
+    {
+        if (direction == OrderDirection.Ascending)
+        {
+            return query.OrderBy(keySelector);
+        }
+
+        if (direction == OrderDirection.Descending)
+        {
+            return query.OrderByDescending(keySelector);
+        }
+
+        throw new InvalidOperationException("Invalid order direction.");
+    }
+
+    /// <summary>
+    /// Orders the query by the specified key selector and direction.
+    /// </summary>
+    public static IOrderedQueryable<TSource> ThenBy<TSource, TKey>(this IOrderedQueryable<TSource> query, Expression<Func<TSource, TKey>> keySelector, OrderDirection direction)
+    {
+        if (direction == OrderDirection.Ascending)
+        {
+            return query.ThenBy(keySelector);
+        }
+
+        if (direction == OrderDirection.Descending)
+        {
+            return query.ThenByDescending(keySelector);
+        }
+
+        throw new InvalidOperationException("Invalid order direction.");
+    }
+}
