57bb8ea5ca9f82bf5bed0b079fc1aa720bd38ae8
Yuriy Durov <u.a.durov@gmail.com>
2024-05-03 18:42:08 +0400

    Add OrderDirection.Reverse()

--- src/BitzArt.LinqExtensions/Enums/OrderDirection.cs
+++ src/BitzArt.LinqExtensions/Enums/OrderDirection.cs
@@ -15,3 +15,25 @@ public enum OrderDirection : byte
     /// </summary>
     Descending = 1
 }
+
+/// <summary>
+/// Extensions to <see cref="OrderDirection"/>.
+/// </summary>
+public static class OrderDirectionExtensions
+{
+    /// <summary>
+    /// Reverse the order direction.
+    /// </summary>
+    /// <param name="orderDirection"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public static OrderDirection Reverse(this OrderDirection orderDirection)
+    {
+        return orderDirection switch
+        {
+            OrderDirection.Ascending => OrderDirection.Descending,
+            OrderDirection.Descending => OrderDirection.Ascending,
+            _ => throw new ArgumentOutOfRangeException(nameof(orderDirection), orderDirection, null)
+        };
+    }
+}
