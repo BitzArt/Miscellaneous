9933bafce72fa3cc5a5e09ae6bb9bb09a9b38d97
Yuriy Durov <u.a.durov@gmail.com>
2024-07-18 15:30:04 +0400

    More overloads for shuffle

--- src/BitzArt.LinqExtensions/Extensions/ShuffleExtensions.cs
+++ src/BitzArt.LinqExtensions/Extensions/ShuffleExtensions.cs
@@ -15,16 +15,33 @@ public static class ShuffleExtensions
     /// Shuffles the elements of the collection
     /// using <see href="https://en.wikipedia.org/wiki/Fisher-Yates_shuffle"/>
     /// </summary>
+    /// <param name="source">The collection to shuffle.</param>
+    /// <param name="seed">The seed to use for the random number generator.</param>
     /// <returns>A new IQueryable with the elements shuffled.</returns>
-    public static IQueryable<TSource> Shuffle<TSource>(this IQueryable<TSource> source)
-        => source.Shuffle().AsQueryable();
+    public static IQueryable<TSource> Shuffle<TSource>(this IQueryable<TSource> source, int? seed = null)
+        => source.Shuffle(seed).AsQueryable();
 
     /// <summary>
     /// Shuffles the elements of the collection
     /// using <see href="https://en.wikipedia.org/wiki/Fisher-Yates_shuffle"/>
     /// </summary>
+    /// <param name="source">The collection to shuffle.</param>
+    /// <param name="seed">The seed to use for the random number generator.</param>
     /// <returns>A new collection with the elements shuffled.</returns>
-    public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+    public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, int? seed = null)
+    {
+        var random = seed.HasValue ? new Random(seed.Value) : _random;
+        return source.Shuffle(random);
+    }
+
+    /// <summary>
+    /// Shuffles the elements of the collection
+    /// using <see href="https://en.wikipedia.org/wiki/Fisher-Yates_shuffle"/>
+    /// </summary>
+    /// <param name="source">The collection to shuffle.</param>
+    /// <param name="random">The random number generator to use.</param>
+    /// <returns>A new collection with the elements shuffled.</returns>
+    public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Random random)
     {
         // preserve the original collection
         var list = new List<T>(source);
@@ -33,7 +50,7 @@ public static class ShuffleExtensions
         while (n > 1)
         {
             n--;
-            var k = _random.Next(n + 1);
+            var k = random.Next(n + 1);
             (list[n], list[k]) = (list[k], list[n]);
         }
         return list;
