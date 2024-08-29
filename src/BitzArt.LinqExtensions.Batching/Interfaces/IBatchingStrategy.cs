a6c94b8b4b23c603bf5f087ee8918bdffe2097a6
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2024-08-29 08:44:59 +0400

    Fix typo

--- src/BitzArt.LinqExtensions.Batching/Interfaces/IBatchingStrategy.cs
+++ src/BitzArt.LinqExtensions.Batching/Interfaces/IBatchingStrategy.cs
@@ -6,7 +6,7 @@
 public interface IBatchingStrategy<TSource>
 {
     /// <summary>
-    /// Return a bath query composed by this strategy.
+    /// Return a batch query composed by this strategy.
     /// </summary>
     public IQueryable<TSource> GetQuery(IQueryable<TSource> query, int size);
 
