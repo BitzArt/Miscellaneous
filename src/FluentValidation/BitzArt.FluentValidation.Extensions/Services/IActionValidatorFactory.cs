315909f7a415fcbdb1c9d51bb59c65793fc7054a
Yuriy Durov <u.a.durov@gmail.com>
2024-10-09 10:51:35 +0400

    IActionValidatorFactory (#8)

---
+++ src/FluentValidation/BitzArt.FluentValidation.Extensions/Services/IActionValidatorFactory.cs
@@ -0,0 +1,11 @@
+﻿
+namespace FluentValidation;
+
+public interface IActionValidatorFactory
+{
+    public IActionValidator<T> GetValidator<T>(ActionType actionType);
+
+    public IActionValidator GetValidator(Type objectType, ActionType actionType);
+
+    internal IActionValidator GetValidatorInternal(Type validatorType, Func<IServiceProvider, ActionType?> getActionType, ActionType? actionType = null);
+}
