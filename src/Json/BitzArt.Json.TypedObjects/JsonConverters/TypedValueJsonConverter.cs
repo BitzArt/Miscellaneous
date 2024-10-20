08300326bbbbe46beb4a1b4fa69c71028dd7db66
Yuriy Durov <u.a.durov@gmail.com>
2024-10-20 18:36:35 +0400

    Add BitzArt.Json.TypedObjects (#11)

---
+++ src/Json/BitzArt.Json.TypedObjects/JsonConverters/TypedValueJsonConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BitzArt.Json;
+
+internal class TypedValueJsonConverter : JsonConverter<TypedObjectJsonValue?>
+{
+    public override TypedObjectJsonValue? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var jsonDocument = JsonDocument.ParseValue(ref reader);
+        var rootElement = jsonDocument.RootElement;
+
+        if (rootElement.ValueKind == JsonValueKind.Null) return null;
+
+        var typeName = rootElement.GetProperty("type").GetString()!;
+        var type = Type.GetType(typeName)!;
+
+        var value = rootElement.GetProperty("value").Deserialize(type, options)!;
+
+        return new TypedObjectJsonValue(typeName, value);
+    }
+
+    public override void Write(Utf8JsonWriter writer, TypedObjectJsonValue? value, JsonSerializerOptions options)
+    {
+        var converter = (JsonConverter<TypedObjectJsonValue?>)options.GetConverter(typeof(TypedObjectJsonValue?));
+        converter.Write(writer, value, options);
+    }
+}
---
+++ tests/Json/BitzArt.Json.TypedObjects.Tests/TypedObjectJsonConverterTests.cs
@@ -0,0 +1,75 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BitzArt.Json.TypedObjects.Tests;
+
+public class TypedObjectJsonConverterTests
+{
+    [Fact]
+    public void Deserialize_WhenNonNullableObject_ShouldRetainOriginalTypes()
+    {
+        // Arrange
+        var testClass = new TestClass1([1, 2L, 3.0f, 4.0, 5.0m, "6", Guid.NewGuid(), true, false]);
+        var types = testClass.Values!.Select(x => x.GetType()).ToList();
+
+        var serialized = JsonSerializer.Serialize(testClass);
+
+        // Act
+        var deserialized = JsonSerializer.Deserialize<TestClass1>(serialized)!;
+
+        // Assert
+        var deserializedTypes = deserialized.Values!.Select(x => x.GetType()).ToList();
+
+        Assert.Equal(testClass.Values, deserialized.Values);
+        Assert.Equal(types, deserializedTypes);
+    }
+
+    private class TestClass1
+    {
+        [JsonConverter(typeof(ItemConverter<TypedObjectJsonConverter<object>>))]
+        public IEnumerable<object>? Values { get; set; }
+
+        public TestClass1(IEnumerable<object> values)
+        {
+            Values = values;
+        }
+
+        public TestClass1()
+        {
+        }
+    }
+
+    [Fact]
+    public void Deserialize_WhenNullableObject_ShouldRetainOriginalTypes()
+    {
+        // Arrange
+        var testClass = new TestClass2([1, 2L, 3.0f, 4.0, 5.0m, "6", Guid.NewGuid(), true, false, null]);
+        var types = testClass.Values!.Select(x => x?.GetType()).ToList();
+
+        var serialized = JsonSerializer.Serialize(testClass);
+
+        // Act
+        var deserialized = JsonSerializer.Deserialize<TestClass2>(serialized)!;
+
+        // Assert
+        var deserializedTypes = deserialized.Values!.Select(x => x?.GetType()).ToList();
+
+        Assert.Equal(testClass.Values, deserialized.Values);
+        Assert.Equal(types, deserializedTypes);
+    }
+
+    private class TestClass2
+    {
+        [JsonConverter(typeof(ItemConverter<TypedObjectJsonConverter<object?>>))]
+        public IEnumerable<object?>? Values { get; set; }
+
+        public TestClass2(IEnumerable<object?> values)
+        {
+            Values = values;
+        }
+
+        public TestClass2()
+        {
+        }
+    }
+}
