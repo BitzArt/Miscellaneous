203b35753e5220f2d0238cadc1718ce5bceadb0c
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2024-10-21 16:09:44 +0400

    Add BitzArt.CoreExtensions

---
+++ src/Misc/BitzArt.CoreExtensions/Extensions/TaskExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace BitzArt;
+
+/// <summary>
+/// Extensions for <see cref="Task"/>.
+/// </summary>
+public static class TaskExtensions
+{
+    /// <summary>
+    /// Executes the <see cref="Task"/> and ignores cancellation exceptions.
+    /// </summary>
+    public static async Task IgnoreCancellationAsync(Task task)
+    {
+        try
+        {
+            await task;
+        }
+        catch
+        {
+            if (task.IsCanceled) return;
+            throw;
+        }
+    }
+}
