2a3595b0481f8e5a16d44c675edc62dc3ce4eccc
Yuriy Durov <u.a.durov@gmail.com>
2024-10-21 19:28:19 +0400

    ActionType override

--- src/FluentValidation/BitzArt.FluentValidation.Extensions/Interfaces/IActionValidator.cs
+++ src/FluentValidation/BitzArt.FluentValidation.Extensions/Interfaces/IActionValidator.cs
@@ -1,11 +1,19 @@
 ﻿namespace FluentValidation;
 
+/// <inheritdoc cref="IActionValidator"/>
+/// <typeparam name="T">The type of object being validated.</typeparam>
 public interface IActionValidator<T> : IValidator<T>, IActionValidator
 {
 
 }
 
+/// <summary>
+/// An <see cref="IValidator"/> that can be used to validate an object based on the action being performed.
+/// </summary>
 public interface IActionValidator
 {
+    /// <summary>
+    /// Current action type.
+    /// </summary>
     public ActionType? Action { get; internal set; }
 }
