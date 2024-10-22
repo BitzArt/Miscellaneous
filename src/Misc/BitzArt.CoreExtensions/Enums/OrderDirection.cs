216acc7ee5fd3b86982d03498ffa2b806b3feb2a
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2024-10-22 13:28:14 +0400

    Add BitzArt.CoreExtensions (#12)
    * Add BitzArt.CoreExtensions
    
    * Set TargetFrameworks
    
    * Add ImplicitUsings
    
    * Update TaskExtensions

---
+++ src/Misc/BitzArt.CoreExtensions/Enums/OrderDirection.cs
@@ -0,0 +1,39 @@
+﻿namespace BitzArt;
+
+/// <summary>
+/// Order direction (ascending/descending).
+/// </summary>
+public enum OrderDirection : byte
+{
+    /// <summary>
+    /// Ascending order direction.
+    /// </summary>
+    Ascending = 0,
+
+    /// <summary>
+    /// Descending order direction.
+    /// </summary>
+    Descending = 1
+}
+
+/// <summary>
+/// Extensions to <see cref="OrderDirection"/>.
+/// </summary>
+public static class OrderDirectionExtensions
+{
+    /// <summary>
+    /// Reverse the order direction.
+    /// </summary>
+    /// <param name="orderDirection"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public static OrderDirection Reverse(this OrderDirection orderDirection)
+    {
+        return orderDirection switch
+        {
+            OrderDirection.Ascending => OrderDirection.Descending,
+            OrderDirection.Descending => OrderDirection.Ascending,
+            _ => throw new ArgumentOutOfRangeException(nameof(orderDirection), orderDirection, null)
+        };
+    }
+}
---
+++ src/Misc/BitzArt.CoreExtensions/Extensions/TaskExtensions.cs
@@ -0,0 +1,51 @@
+﻿namespace BitzArt;
+
+/// <summary>
+/// Extension methods for <see cref="Task"/>.
+/// </summary>
+public static class TaskExtensions
+{
+    /// <summary>
+    /// Awaits the <paramref name="task"/> until it is completed or cancelled, while ignoring cancellation exceptions.
+    /// </summary>
+    /// <param name="task">The task to await.</param>
+    /// <param name="ignoreCancellation">Whether to ignore cancellation exceptions.</param>
+    /// <param name="byTaskStatus">Determines whether to check for task cancellation by task status (if <see langword="true"/>) or by <see cref="OperationCanceledException"/> (if <see langword="false"/>).</param>
+    public static Task IgnoreCancellation(this Task task, bool ignoreCancellation = true, bool byTaskStatus = true)
+    {
+        if (!ignoreCancellation) return task;
+
+        return byTaskStatus
+            ? IgnoreCancellationByTaskCancelledAsync(task)
+            : IgnoreCancellationByExceptionAsync(task);
+    }
+
+    private static async Task IgnoreCancellationByTaskCancelledAsync(Task task)
+    {
+        try
+        {
+            await task;
+        }
+        catch
+        {
+            if (task.IsCanceled) return;
+            throw;
+        }
+    }
+
+    private static async Task IgnoreCancellationByExceptionAsync(Task task)
+    {
+        try
+        {
+            await task;
+        }
+        catch (OperationCanceledException)
+        {
+            return;
+        }
+        catch
+        {
+            throw;
+        }
+    }
+}
