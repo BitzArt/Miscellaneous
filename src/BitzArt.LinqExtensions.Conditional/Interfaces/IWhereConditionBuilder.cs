6f15fa715b300f572850617d6d9e44d5d41d5526
Yuriy Durov <u.a.durov@gmail.com>
2025-03-11 18:48:54 +0400

    Add LinqExtensions.Conditional

---
+++ src/BitzArt.LinqExtensions.Conditional/Interfaces/IWhereConditionBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace BitzArt.Linq.Conditional;
+
+/// <summary>
+/// Builder for a complex 'Where' condition.
+/// </summary>
+/// <typeparam name="TSource"></typeparam>
+/// <typeparam name="TMember"></typeparam>
+public interface IWhereConditionBuilder<TSource, TMember> : IQueryable<TSource>
+{
+    internal IQueryable<TSource> Query { get; set; }
+    internal Expression<Func<TSource, TMember>> Selector { get; set; }
+}
---
+++ src/BitzArt.LinqExtensions.Conditional/Utility/ExpressionCombiner.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace System.Linq;
+
+internal static class ExpressionCombiner
+{
+    public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> exp, Expression<Func<T, bool>> newExp)
+    {
+        var visitor = new ParameterUpdateVisitor(newExp.Parameters.First(), exp.Parameters.First());
+        newExp = (visitor.Visit(newExp) as Expression<Func<T, bool>>)!;
+
+        var binExp = Expression.And(exp.Body, newExp.Body);
+        return Expression.Lambda<Func<T, bool>>(binExp, newExp.Parameters);
+    }
+
+    private class ParameterUpdateVisitor(ParameterExpression oldParameter, ParameterExpression newParameter) : ExpressionVisitor
+    {
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            if (object.ReferenceEquals(node, oldParameter))
+                return newParameter;
+
+            return base.VisitParameter(node);
+        }
+    }
+}
