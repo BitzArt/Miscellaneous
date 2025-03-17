716c474e3a2b05c6f045bec43dd7a7b76b82bc91
Yuriy Durov <u.a.durov@gmail.com>
2025-03-17 23:26:42 +0400

    Fix IsNullable extension

--- src/Misc/BitzArt.CoreExtensions/Extensions/TypeExtensions.cs
+++ src/Misc/BitzArt.CoreExtensions/Extensions/TypeExtensions.cs
@@ -1,12 +1,18 @@
 ﻿namespace BitzArt;
 
-internal static class TypeExtensions
+/// <summary>
+/// Provides a set of extension methods for <see cref="Type"/>.
+/// </summary>
+public static class TypeExtensions
 {
+    /// <summary>
+    /// Determines whether the specified type is nullable.
+    /// </summary>
     public static bool IsNullable(this Type type, out Type? underlyingType)
     {
         underlyingType = null;
 
-        if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>) == false)
+        if ((type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>)) == false)
             return false;
 
         underlyingType = Nullable.GetUnderlyingType(type);
