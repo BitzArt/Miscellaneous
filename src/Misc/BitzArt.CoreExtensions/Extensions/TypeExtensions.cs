c1a60b57a16fe372750f2cbea3ab7583c2a37bd2
Yuriy Durov <u.a.durov@gmail.com>
2025-03-17 16:43:00 +0400

    Add out Type? underlyingType to IsNullable extension

--- src/Misc/BitzArt.CoreExtensions/Extensions/TypeExtensions.cs
+++ src/Misc/BitzArt.CoreExtensions/Extensions/TypeExtensions.cs
@@ -2,6 +2,15 @@
 
 internal static class TypeExtensions
 {
-    public static bool IsNullable(this Type type)
-        => type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
+    public static bool IsNullable(this Type type, out Type? underlyingType)
+    {
+        underlyingType = null;
+
+        if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>) == false)
+            return false;
+
+        underlyingType = Nullable.GetUnderlyingType(type);
+
+        return true;
+    }
 }
