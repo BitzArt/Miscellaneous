2c2dce1eba5b5fcb3ad09b8b963ab679402ceda2
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-05-05 23:29:59 +0300

    Add messaging configuration and project references for sample apps

--- sample/Wolverine/BitzArt.Wolverine.Extensions.Sample.Processor/Program.cs
+++ sample/Wolverine/BitzArt.Wolverine.Extensions.Sample.Processor/Program.cs
@@ -1,6 +1,39 @@
+using System.Collections.Immutable;
+using System.Reflection;
+using BitzArt.Extensions;
+using BitzArt.Wolverine.Extensions.AzureServiceBus;
+using BitzArt.Wolverine.Extensions.RabbitMq;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMessaging(
+    configuration: builder.Configuration,
+    configure: messaging =>
+    {
+        // Just for demonstration purposes - how you can use Wolverine's options
+        messaging.WolverineOptions.UseNewtonsoftForSerialization();
+        
+        messaging.AddBus("my-bus-1", bus =>
+        {
+            bus.Topic("some-topic").ToQueue("some-queue");
+            
+            bus.ConfigureRabbitMq(rabbit =>
+            {
+                
+            });
+
+            bus.ConfigureAzureServiceBus(azure =>
+            {
+                
+            });
+        });
+    },
+    assemblies: ImmutableList<Assembly>.Empty);
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
 
 app.Run();
