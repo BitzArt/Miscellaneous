214c19fb5b5a9077c50faef1543e0a79848f81fb
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-05-08 22:12:31 +0300

    Update sample

--- sample/Wolverine/BitzArt.Wolverine.Extensions.Sample.Processor/Program.cs
+++ sample/Wolverine/BitzArt.Wolverine.Extensions.Sample.Processor/Program.cs
@@ -1,6 +1,31 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using BitzArt;
+using BitzArt.Messages;
+using BitzArt.Wolverine.Extensions.Sample.Processor;
+using Wolverine.AzureServiceBus;
+using Wolverine.RabbitMQ;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMessaging(
+    configuration: builder.Configuration,
+    configure: configuration =>
+    {
+        configuration.ConfigureRabbitMq((wolverine, rabbitMq) =>
+        {
+            rabbitMq.BindExchange("msg").ToQueue("messages");
+
+            wolverine.ListenToRabbitQueue("messages");
+        });
+
+        configuration.ConfigureAzureServiceBus((wolverine, serviceBus) =>
+        {
+            wolverine.ListenToAzureServiceBusSubscription("messages").FromTopic("msg");
+        });
+    }, assemblies:
+    [
+        typeof(MyMessageHandler).Assembly
+    ]);
+
+var app = builder.Build();
 
 app.Run();
--- sample/Wolverine/BitzArt.Wolverine.Extensions.Sample.Producer/Program.cs
+++ sample/Wolverine/BitzArt.Wolverine.Extensions.Sample.Producer/Program.cs
@@ -1,6 +1,57 @@
+using BitzArt;
+using BitzArt.Messages;
+using BitzArt.Wolverine.Extensions.Sample.Common;
+using Wolverine;
+using Wolverine.AzureServiceBus;
+using Wolverine.RabbitMQ;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMessaging(
+    configuration: builder.Configuration,
+    configure: configuration =>
+    {
+        configuration.ConfigureRabbitMq((wolverine, rabbitMq) =>
+        {
+            wolverine.PublishMessage<MyMessage>().ToRabbitExchange("msg");
+            wolverine.PublishMessage<MyRequest>().ToRabbitExchange("ping-pong");
+        });
+
+        configuration.ConfigureAzureServiceBus((wolverine, serviceBus) =>
+        {
+            wolverine.PublishMessage<MyMessage>().ToAzureServiceBusTopic("msg");
+            wolverine.PublishMessage<MyRequest>().ToAzureServiceBusTopic("ping-pong");
+        });
+    });
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/message", (string value, IMessageBus bus) =>
+{
+    MyMessage message = new MyMessage
+    {
+        Value = value
+    };
+    
+    bus.PublishAsync(message);
+    
+    Console.WriteLine("Message published");
+});
+
+app.MapGet("/ping", async (string value, IMessageBus bus) =>
+{
+    MyRequest request = new MyRequest
+    {
+        Value = value
+    };
+
+    var response = await bus.InvokeAsync<ResponseMessage<MyResponse>>(request);
+    
+    Console.WriteLine($"Response: {response.Data.Value}");
+    
+});
 
 app.Run();
