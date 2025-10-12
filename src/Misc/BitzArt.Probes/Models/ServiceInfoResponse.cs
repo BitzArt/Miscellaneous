a03488b38450f02920c35834e41f9eed87932f6a
Yuriy Durov <yu.durov@gmail.com>
2025-10-12 15:55:49 +0400

    Add service uptime info in ServiceInfoResponse

--- src/Misc/BitzArt.Probes/Models/ServiceInfoResponse.cs
+++ src/Misc/BitzArt.Probes/Models/ServiceInfoResponse.cs
@@ -2,14 +2,32 @@
 
 namespace BitzArt.Probes;
 
-internal class ServiceInfoResponse(ServiceInfoOptions? options, string? environment)
+internal class ServiceInfoResponse
 {
     [JsonPropertyName("name")]
-    public string? Name { get; set; } = options?.Name;
+    public string? Name { get; set; }
 
     [JsonPropertyName("version")]
-    public string? Version { get; set; } = options?.Version;
+    public string? Version { get; set; }
 
     [JsonPropertyName("environment")]
-    public string? Environment { get; set; } = environment;
+    public string? Environment { get; set; }
+
+    [JsonPropertyName("startedAt")]
+    public DateTimeOffset StartedAt { get; set; }
+
+    [JsonPropertyName("currentTime")]
+    public static DateTimeOffset CurrentTime => DateTimeOffset.UtcNow;
+
+    [JsonPropertyName("uptime")]
+    public TimeSpan Uptime => CurrentTime - StartedAt;
+
+    public ServiceInfoResponse(ServiceInfoOptions? options, string? environment)
+    {
+        Name = options?.Name;
+        Version = options?.Version;
+        Environment = environment;
+
+        StartedAt = DateTimeOffset.UtcNow;
+    }
 }
